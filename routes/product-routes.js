0f7612bb53c7e664c1986f2c93a5ce0d6d6a33ba
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-07-20 01:22:35 +0530

    Add product stats route to product routes

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -1,8 +1,9 @@
 const express = require('express'); 
-const { getAllProducts, addProducts } = require('../controllers/product-controller');
+const { getAllProducts, addProducts, getProductStats } = require('../controllers/product-controller');
 const router = express.Router(); 
 
 router.get('/all', getAllProducts); 
 router.post('/add', addProducts); 
+router.get('/stats', getProductStats);
 
 module.exports = router;
