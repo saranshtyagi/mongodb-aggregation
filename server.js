13d81c343c21f2685da4256d0324db4fc2d79d0d
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-07-20 00:50:23 +0530

    Add product routes integration in server setup

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require('express'); 
 const connectToDB = require("./db/db");
 const app = express(); 
+const productRoutes = require('./routes/product-routes');
 
 const PORT = process.env.PORT || 3000; 
 
 connectToDB();
 
 app.use(express.json());
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
