062c00b4a7bde126eebe32f91b529d06454ee6ba
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-07-21 11:09:37 +0530

    Add analysis route to product routes for revenue and price statistics

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -1,9 +1,10 @@
 const express = require('express'); 
-const { getAllProducts, addProducts, getProductStats } = require('../controllers/product-controller');
+const { getAllProducts, addProducts, getProductStats, getProductAnalysis } = require('../controllers/product-controller');
 const router = express.Router(); 
 
 router.get('/all', getAllProducts); 
 router.post('/add', addProducts); 
 router.get('/stats', getProductStats);
+router.get('/analysis', getProductAnalysis);
 
 module.exports = router;
