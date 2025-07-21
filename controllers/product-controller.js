1c68867b841b7bdf254668a82da5a7dfdf0a9476
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-07-21 11:09:25 +0530

    Add getProductAnalysis function to calculate revenue and price statistics for Electronics category

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -88,8 +88,60 @@ const getProductStats = async(req, res) => {
     }
 }
 
+const getProductAnalysis = async(req, res) => {
+    try {
+        const result = await Product.aggregate([
+            {
+                $match: {
+                    category: 'Electronics'
+                }
+            }, 
+            {
+                $group: {
+                    _id: null, 
+                    totalRevenue: {
+                        $sum: "$price"
+                    },
+                    averagePrice: {
+                        $avg: "$price"
+                    }, 
+                    maxProductPrice: {
+                        $max: "$price"
+                    }, 
+                    minProductPrice: {
+                        $min: "$price"
+                    }
+                }
+            }, 
+            {
+                $project: { // 0 and 1 are boolean 
+                    _id: 0, 
+                    totalRevenue: 1, 
+                    averagePrice: 1, 
+                    maxProductPrice: 1, 
+                    minProductPrice: 1, 
+                    priceRange: {
+                        $subtract: ["$maxProductPrice", "$minProductPrice"]
+                    }
+                }
+            }
+        ]);
+        res.status(200).json({
+            success: true, 
+            data: result
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false, 
+            message: 'Something went wrong. Please try again!', 
+            error: error.message
+        });
+    }
+}
+
 module.exports = {
     getAllProducts, 
     addProducts,
-    getProductStats
+    getProductStats, 
+    getProductAnalysis
 }
