24e236b47353b1281a0e4fdc6dac5fc0089ae888
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-07-21 11:42:28 +0530

    Add book routes to server for handling reference API

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connectToDB = require("./db/db");
 const app = express(); 
 const productRoutes = require('./routes/product-routes');
+const bookRoutes = require('./routes/book-routes');
 
 const PORT = process.env.PORT || 3000; 
 
@@ -10,6 +11,7 @@ connectToDB();
 
 app.use(express.json());
 app.use('/api/products', productRoutes);
+app.use('/api/reference', bookRoutes);
 
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
